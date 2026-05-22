698cde05d978d41ae2173a4aed139d32b4ebe09c
joaogarcianlhpd <joaogarciasilva9@gmail.com>
2026-05-22 18:02:12 -0300

    Commit Inicial

---
+++ api-crud-pedidos/src/main/java/br/com/apicrudpedidos/ApiCrudPedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.apicrudpedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudPedidosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCrudPedidosApplication.class, args);
+    }
+
+}
---
+++ api-crud-pedidos/src/test/java/br/com/apicrudpedidos/ApiCrudPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apicrudpedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudPedidosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
